f8ab728c5c2dc69eae9105a989312165c621b4f6
Stacey Gennoy <sgennoy@gmail.com>
2015-11-01 15:58:04 -0800

    Created cocoa view controllers to segue and added assets

---
+++ DropboxSG/CreateAccountViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CreateAccountViewController.swift
+//  DropboxSG
+//
+//  Created by Stacey Gennoy on 11/1/15.
+//  Copyright © 2015 Stacey Gennoy. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

51490a8fbced60f38d36fc512adc98d08e6cd634
Stacey Gennoy <sgennoy@gmail.com>
2015-11-01 21:04:46 -0800

    Settings button for sign out to send user to sign in

---
+++ DropboxSG/SettingsViewController.swift
@@ -0,0 +1,41 @@
+//
+//  SettingsViewController.swift
+//  DropboxSG
+//
+//  Created by Stacey Gennoy on 11/1/15.
+//  Copyright © 2015 Stacey Gennoy. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    
+        scrollView.contentSize = imageView.image!.size
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

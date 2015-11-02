7c7ba417c684db2a2bea92cc9d1f00a882824c3e
Stacey Gennoy <sgennoy@gmail.com>
2015-11-01 16:13:05 -0800

    Added cocoa touch class to sign in seque

---
+++ DropboxSG/SignInRootViewController.swift
@@ -0,0 +1,37 @@
+//
+//  SignInRootViewController.swift
+//  DropboxSG
+//
+//  Created by Stacey Gennoy on 11/1/15.
+//  Copyright © 2015 Stacey Gennoy. All rights reserved.
+//
+
+import UIKit
+
+class SignInRootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        performSegueWithIdentifier("signInSegue", sender: self)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
